81c8055ac9c3ae52045edc25f844b16ee251973a
Surabhi Thakur <surabhithakur.33@gmail.com>
2023-12-29 15:47:20 +0530

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white">
+      <div className="container mx-auto px-4 sm:px-6 md:px-8">
+        <nav className="flex items-center justify-between h-16">
+          {/* Logo or Brand */}
+          <div className="flex-shrink-0">
+            <img src="/logo.png" alt="Logo" className="h-10" />
+          </div>
+
+          {/* Navigation Links */}
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Services
+              </a>
+            </li>
+            {/* Add more navigation links as needed */}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
